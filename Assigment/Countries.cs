06fe746e30597e773a84b0df377ed815fe2bdbed
Ivan Popov <unfragable@data.bg>
2015-02-23 22:48:29 +0200

    Initial commit

---
+++ Assigment/Countries.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+class Countries
+{
+    private string name;
+    private double gdpGrowth;
+    private double inflation;
+    private double tradeBalance;
+    private int hdiRank;
+    private string[] tradePartners = new string[5];
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public double GdpGrowth
+    {
+        get { return gdpGrowth; }
+        set { gdpGrowth = value; }
+    }
+
+    public double Inflation
+    {
+        get { return inflation; }
+        set { inflation = value; }
+    }
+
+    public double TradeBalance
+    {
+        get { return tradeBalance; }
+        set { tradeBalance = value; }
+    }
+
+    public int HdiRank
+    {
+        get { return hdiRank; }
+        set { hdiRank = value; }
+    }
+
+    public string[] TradePartners
+    {
+        get { return tradePartners; }
+        set { tradePartners = value; }
+    }
+}
---
+++ Assigment/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace fileReadExample
+{
+    class Program
+    {      
+        static string[] headers = new string[6];	//column	headers
+
+        static void Main(string[] args)
+        {
+            const int MAX_LINES_FILE = 50000;
+            string[] AllLines = new string[MAX_LINES_FILE];
+            List<Countries> countries = new List<Countries>();
+
+            //reads	from	bin/DEBUG	subdirectory	of	project	directory
+            AllLines = File.ReadAllLines("countries.csv");
+
+            
+
+            foreach (string line in AllLines)
+            {
+                if (line.StartsWith("Country"))	//found	first	line	- headers	
+                {
+                    headers = line.Split(',');
+                }
+                else
+                {
+                    int index = 0;
+
+                    //split	data	using	commas
+                    string[] columns = line.Split(',');
+                    countries.Add(new Countries { Name = columns[0], GdpGrowth = Convert.ToDouble(columns[1]), 
+                        Inflation = Convert.ToDouble(columns[2]), TradeBalance = Convert.ToDouble(columns[3]), 
+                        HdiRank = Int32.Parse(columns[4]), TradePartners = null });
+
+                    string[] partners = columns[5].Split(';', '[', ']');
+                    foreach (string tradePartner in partners)
+                    {
+                        if (tradePartner != "")
+                        {
+                            countries[index].TradePartners[0] = new string()
+                            countries[index].TradePartners[0] = "ad";
+                        }
+                    }
+                    index++;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
